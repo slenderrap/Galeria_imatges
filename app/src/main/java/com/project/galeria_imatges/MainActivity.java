624702b3c26aed637278eae721b40f10052c2bf8
Oriol Arribas <152431522+slenderrap@users.noreply.github.com>
2024-11-27 20:50:33 +0100

    push

--- app/src/main/java/com/project/galeria_imatges/MainActivity.java
+++ app/src/main/java/com/project/galeria_imatges/MainActivity.java
@@ -24,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
