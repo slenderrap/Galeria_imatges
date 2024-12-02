e10c248463c451081de17e71c105b4b13f2695fe
Oriol Arribas <152431522+slenderrap@users.noreply.github.com>
2024-12-02 17:11:26 +0100

    Amb la camera no acaba de funcionar

--- app/src/main/java/com/project/galeria_imatges/MainActivity.java
+++ app/src/main/java/com/project/galeria_imatges/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,21 +15,19 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     public static int RC_PHOTO_PICKER = 0;
-    ActivityResultLauncher<Intent> someActivityResultLauncher;
+    ActivityResultLauncher<Intent> galerySomeActivityResultLauncher;
+    ActivityResultLauncher<Intent> cameraSomeActivityResultLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+
+        //afegim result per la galeria
+        galerySomeActivityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
@@ -43,23 +42,49 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        //fem boto per cridar a la galeria
+        Button buttonGaleria = findViewById(R.id.buttonGalery);
+        buttonGaleria.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Create Intent
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                Intent intent = new Intent(Intent.ACTION_PICK);
+//                intent.getAction(getDataDir(new Uri ));
                 intent.setType("image/jpg");
                 intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
                 //Launch activity to get result
-                someActivityResultLauncher.launch(intent);
+                galerySomeActivityResultLauncher.launch(intent);
             }
         });
+        Button buttonCamera = findViewById(R.id.buttonCamera);
+    //afegim result camera
+        cameraSomeActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            Uri uri = data.getData();
+                            ImageView imageView = findViewById(R.id.imageView);
+                            imageView.setImageURI(uri);
+
+                        }}
+
+                }
+        );
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Create Intent
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //Launch activity to get result
+                cameraSomeActivityResultLauncher.launch(intent);
+            }
+                }
+        );
 
     }
-
-
 }
 
 
